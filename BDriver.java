4724da8428eb5f76334c8baae81344ab0c6d1d6c
Neeraj Verma <113288731+NeerajVerma2001@users.noreply.github.com>
2023-06-03 18:49:19 +0530

    added BDriver.java
    Inheritance program

---
+++ BDriver.java
@@ -0,0 +1,12 @@
+package oops;
+
+public class BDriver 
+{
+	public static void main(String[] args) {
+		B b=new B();
+		System.out.println(b.name);
+		System.out.println(b.age);
+		b.test();
+		b.demo();
+	}
+}
